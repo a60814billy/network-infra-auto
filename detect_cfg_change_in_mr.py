2110103740cb8e49a1eb83aeaf0c50ec789d5c2a
BoHong Li <a60814billy@gmail.com>
2025-03-14 10:58:00 +0800

    ci: detect script on main branch

--- detect_cfg_change_in_mr.py
+++ detect_cfg_change_in_mr.py
@@ -5,27 +5,40 @@ import requests
 gitlab_api_url = "http://10.192.4.172/api/v4"
 
 project_id = os.environ["CI_PROJECT_ID"]
-merge_request_iid = os.environ["CI_MERGE_REQUEST_IID"]
+merge_request_iid = os.environ.get("CI_MERGE_REQUEST_IID", None)
 api_token = os.environ["GITLAB_API_TOKEN"]
 
-resp = requests.get(
-    f'{gitlab_api_url}/projects/{project_id}/merge_requests/{merge_request_iid}/changes',
-    headers={'PRIVATE-TOKEN': api_token}
-)
 
-if resp.status_code != 200:
-    raise Exception(f'Failed to get changes: {resp.text}')
+def get_mr_change_files():
+    resp = requests.get(
+        f'{gitlab_api_url}/projects/{project_id}/merge_requests/{merge_request_iid}/changes',
+        headers={'PRIVATE-TOKEN': api_token}
+    )
 
-changes = resp.json()
+    if resp.status_code != 200:
+        raise Exception(f'Failed to get changes: {resp.text}')
 
-changeset = changes['changes']
+    changes = resp.json()
 
-device_parse_re = re.compile(r'^cfg/(.*)\.cfg$')
+    changeset = changes['changes']
 
-for change in changeset:
-    # if new_path is in cfg/
-    if change['new_path'].startswith('cfg/'):
-        # use filename to get the device host
-        device_parse_re.search(change['new_path'])
-        device = device_parse_re.search(change['new_path']).group(1)
-        print(device)
+    device_parse_re = re.compile(r'^cfg/(.*)\.cfg$')
+
+    for change in changeset:
+        # if new_path is in cfg/
+        if change['new_path'].startswith('cfg/'):
+            # use filename to get the device host
+            device_parse_re.search(change['new_path'])
+            device = device_parse_re.search(change['new_path']).group(1)
+            print(device)
+
+def get_merged_mr_changes():
+    # use git command to get diff file names
+    diff_cmd = 'git diff --name-only HEAD^1 HEAD cfg/'
+    diff_files = os.popen(diff_cmd).read().splitlines()
+    print(diff_files)
+
+if merge_request_iid:
+    get_mr_change_files()
+else:
+    get_merged_mr_changes()
