ccf40811dd17c66242de6b9da36d48c867036c67
BoHong Li <a60814billy@gmail.com>
2025-03-14 11:25:06 +0800

    feat: detect device

--- detect_cfg_change_in_mr.py
+++ detect_cfg_change_in_mr.py
@@ -1,5 +1,6 @@
 import os
 import re
+
 import requests
 
 gitlab_api_url = "http://10.192.4.172/api/v4"
@@ -7,6 +8,8 @@ gitlab_api_url = "http://10.192.4.172/api/v4"
 project_id = os.environ["CI_PROJECT_ID"]
 merge_request_iid = os.environ.get("CI_MERGE_REQUEST_IID", None)
 api_token = os.environ["GITLAB_API_TOKEN"]
+
+device_parse_re = re.compile(r'^cfg/(.*)\.cfg$')
 
 
 def get_mr_change_files():
@@ -19,28 +22,44 @@ def get_mr_change_files():
         raise Exception(f'Failed to get changes: {resp.text}')
 
     changes = resp.json()
-
     changeset = changes['changes']
 
-    device_parse_re = re.compile(r'^cfg/(.*)\.cfg$')
+    device_list = []
 
     for change in changeset:
         # if new_path is in cfg/
         if change['new_path'].startswith('cfg/'):
             # use filename to get the device host
-            device_parse_re.search(change['new_path'])
             device = device_parse_re.search(change['new_path']).group(1)
-            print(device)
+            device_list.append(device)
+
+    return device_list
+
 
 def get_merged_mr_changes():
     # use git command to get diff file names
     diff_cmd = 'git diff --name-only HEAD^1 HEAD cfg/'
     diff_files = os.popen(diff_cmd).read().splitlines()
-    print(diff_files)
+    device_list = []
+    for file in diff_files:
+        device = device_parse_re.search(file).group(1)
+        device_list.append(device)
 
-if merge_request_iid:
-    print('Getting MR changes')
-    get_mr_change_files()
-else:
-    print('Getting merged MR changes')
-    get_merged_mr_changes()
+    return device_list
+
+
+def main():
+    if merge_request_iid:
+        print('Getting MR changes')
+        device_list = get_mr_change_files()
+    else:
+        print('Getting merged MR changes')
+        device_list = get_merged_mr_changes()
+
+    with open('.change_device_list', 'w') as f:
+        for device in device_list:
+            f.write(f'{device}\n')
+
+
+if __name__ == '__main__':
+    main()
--- nornir_sync_cfg_to_devices.py
+++ nornir_sync_cfg_to_devices.py
@@ -1,9 +1,9 @@
-import difflib
 import argparse
+import os
 
-import napalm
 from nornir import InitNornir
-from nornir.core.task import MultiResult, Result, Task
+from nornir.core.filter import F
+from nornir.core.task import Result, Task
 from nornir_utils.plugins.functions import print_result
 
 nr = InitNornir(config_file="nornir.yaml")
@@ -28,11 +28,12 @@ def sync_cfg_to_device(task: Task, dry_run: bool = False) -> Result:
 
         if conn.compare_config():
             conn.commit_config()
+            # write running to startup
             changed = True
         else:
             changed = False
 
-        return Result(host=task.host, changed=changed)
+        return Result(host=task.host, changed=changed, diff=diff)
 
     finally:
         conn.close()
@@ -43,7 +44,18 @@ def main():
     parser.add_argument("--dry-run", action="store_true", help="Print diff only")
     args = parser.parse_args()
     dry_run = args.dry_run
-    result = nr.run(task=sync_cfg_to_device, dry_run=dry_run)
+
+    device_list = None
+    # check .change_device_list file exists
+    if not os.path.exists('.change_device_list'):
+        print('No device change list found')
+        change_device_set = nr
+    else:
+        with open('.change_device_list', 'r') as f:
+            device_list = f.read().splitlines()
+        change_device_set = nr.filter(F(name__in=device_list))
+
+    result = change_device_set.run(task=sync_cfg_to_device, dry_run=dry_run)
     print_result(result)
 
 if __name__ == '__main__':
