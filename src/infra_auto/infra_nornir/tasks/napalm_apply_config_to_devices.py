621aa7e6c0e2eeb4f0f2ec85f97555d6fa6db45e
BoHong Li <a60814billy@gmail.com>
2025-06-19 15:39:02 +0800

    feat: Add hostname validation in napalm_apply_config_to_devices task

--- src/infra_auto/infra_nornir/tasks/napalm_apply_config_to_devices.py
+++ src/infra_auto/infra_nornir/tasks/napalm_apply_config_to_devices.py
@@ -4,14 +4,53 @@ from nornir.core.task import Result, Task
 from nornir_napalm.plugins.connections import CONNECTION_NAME
 
 
+def check_config_hostname(
+    task: Task, config_path: str, dry_run: Optional[bool] = None
+) -> Result:
+    """
+    Check if the hostname in the configuration file matches the Nornir host name.
+    """
+    with open(config_path, "r") as f:
+        config = f.read()
+
+    configured_hostname = ""
+    if task.host.platform in ["ios", "iosxr", "nxos"]:
+        config = [line.strip() for line in config.splitlines() if line.strip()]
+        for line in config:
+            if line.startswith("hostname "):
+                configured_hostname = line.split(" ")[1]
+                break
+    elif task.host.platform in ["hpe_comware"]:
+        config = [line.strip() for line in config.splitlines() if line.strip()]
+        for line in config:
+            if line.startswith("sysname "):
+                configured_hostname = line.split(" ")[1]
+                break
+    else:
+        raise ValueError(f"Unsupported platform: {task.host.platform}")
+
+    if configured_hostname == "" or configured_hostname != task.host.name:
+        raise ValueError(
+            f"Configured hostname '{configured_hostname}' does not match Nornir hostname '{task.host.name}'"
+        )
+
+    return Result(
+        host=task.host,
+        changed=True,
+        result=f"hostname check passed for {task.host.name}",
+    )
+
+
 def napalm_apply_config_to_devices(
-    task: Task, dry_run: Optional[bool] = False
+    task: Task, dry_run: Optional[bool] = None
 ) -> Result:
     diff = ""
     changed = False
     result = ""
 
     local_cfg_path = f"cfg/{task.host.name}.cfg"
+
+    task.run(task=check_config_hostname, config_path=local_cfg_path)
 
     conn = task.host.get_connection(CONNECTION_NAME, task.nornir.config)
 
